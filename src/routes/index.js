2eec1297098e8ca8fc8f6b180c413f4459391f77
Đặng Thái Sơn <87532573+652001@users.noreply.github.com>
2022-11-27 20:34:25 +0700

    new_table

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,25 +3,21 @@ import Thongke from '~/page/TongQuan/ThongKe';
 import DanhSachNhanVien from '~/page/TongQuan/DanhSachNhanVien';
 import DanhSachTaiKhoan from '~/page/TongQuan/DanhSachTaiKhoan';
 // DanhSach
-import DanhSachPhongBan from '~/page/DanhSach/PhongBan';
+import PhongBan from '~/page/DanhSach/PhongBan';
 import ChucVu from '~/page/DanhSach/ChucVu';
 import TrinhDo from '~/page/DanhSach/TrinhDo';
 import BangCap from '~/page/DanhSach/BangCap';
 import ChuyenMon from '~/page/DanhSach/ChuyenMon';
 import DanhSachHopDong from '~/page/DanhSach/HopDong';
 // Cham Cong
-import DanhSachChamCong from '~/page/DanhSach/DanhSachChamCong';
-<<<<<<< Updated upstream
-import DanhSachHopDong from '~/page/DanhSach/DanhSachHopDong';
-import DanhSachTaiKhoan from '~/page/DanhSach/DanhSachTaiKhoan';
-import DanhSachPhongBan from '~/page/QuanLyPhongBan/DanhSachPhongBan';
-=======
+import DanhSachChamCong from '~/page/DanhSachChamCong';
 // Luong
 import DanhSachBangLuong from '~/page/BangLuong/DanhSachBangLuong';
+
 // He Thong
->>>>>>> Stashed changes
-import ThietLap from '~/page/HeThong/ThietLap';
+
 import CaiDatGioCong from '~/page/HeThong/CaiDatGioCong';
+import ThietLap from '~/page/HeThong/ThietLap';
 
 
 
@@ -32,7 +28,7 @@ const publicRoutes = [
     { path: '/danhsachnhanvien', component: DanhSachNhanVien },
     { path: '/danhsachtaikhoan', component: DanhSachTaiKhoan },
     //Danh Sach
-    { path: '/phongban', component: DanhSachPhongBan },
+    { path: '/phongban', component: PhongBan },
     { path: '/chucvu', component: ChucVu },
     { path: '/trinhdo', component: TrinhDo },
     { path: '/bangcap', component: BangCap },
